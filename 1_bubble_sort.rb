31561361f167bc1f8920708bf1a6ac31918f7723
Carter <carter.hemington@gmail.com>
2023-03-31 11:11:23 -0400

    complete bubble sort

--- 1_bubble_sort.rb
+++ 1_bubble_sort.rb
@@ -1,22 +1,32 @@
 def bubble_sort(array)
-  # for each item, look at item and item directly to the right
-  # if out of order, then swap
+
   swap_count = 0
+
+  # p array
   array.each_with_index do |element, index|
+    # looping through each item in array, and moving item up if its bigger
 
     next_element = array[index+1]
-    # maybe an if statement that asks if we are at the end of the array
 
-    p "element #{element} > next_element #{next_element}"
+    # p "element #{element}, next_element #{next_element}"
 
     if next_element != nil && element > next_element
       swap_count += 1
-      p "swap_count #{swap_count}"
+      # p "swap_count #{swap_count}"
+      array[index], array[index + 1] = array[index + 1], array[index]
       swap_count
-    end 
-    # if swap_count didn't increase then stop looping
+    end   
   end
-p array
+
+  # if swap count is greater than zero, then we need to run it again as there may be swaps to make
+  # if there were no swaps made in last run, then we are done, print the array
+  if swap_count > 0
+    bubble_sort(array)
+  elsif swap_count == 0
+    p array
+  end
+
 end
 
 bubble_sort([4,3,78,2,0,2])
+bubble_sort([10, 2, 6, 2, 1, 5, 8, 6, 0, 2, 8, 9])
