932cf62da8332c0ad854dd93e44417e23aa7ddd1
Carter <carter.hemington@gmail.com>
2023-03-31 10:32:41 -0400

    update bubble sort, better printing to identify flow

--- 1_bubble_sort.rb
+++ 1_bubble_sort.rb
@@ -1,18 +1,22 @@
 def bubble_sort(array)
   # for each item, look at item and item directly to the right
   # if out of order, then swap
-
+  swap_count = 0
   array.each_with_index do |element, index|
-    p "element #{element}, index #{index}"
-    next_index = array[index+1]
-    p "next_index #{next_index}"
 
-    if next_index != nil && element < next_index
-      # swap them
-      p "element #{element}"
-    end
+    next_element = array[index+1]
+    # maybe an if statement that asks if we are at the end of the array
+
+    p "element #{element} > next_element #{next_element}"
+
+    if next_element != nil && element > next_element
+      swap_count += 1
+      p "swap_count #{swap_count}"
+      swap_count
+    end 
+    # if swap_count didn't increase then stop looping
   end
-
+p array
 end
 
 bubble_sort([4,3,78,2,0,2])
